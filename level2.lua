5aeda9ce01bbbde771a0239936c10755380a4592
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-05-25 11:04:48 -0400

    code

--- level2.lua
+++ level2.lua
@@ -4,9 +4,96 @@
 -- Date: May 10th, 2018
 -- Description: 
 
-
 -- Naming scene
-sceneName = "level2.lua" 
 
 
+local composer = require( "composer" )
+local sceneName = "level2.lua" 
+local scene = composer.newScene( sceneName )
 
+---------------------------------------------------------------------------------------
+--LOCAL VARIABLES
+---------------------------------------------------------------------------------------
+local bkg
+
+--------------------------------------
+--PUZZLE FADED
+--------------------------------------
+local pieceF1
+local pieceF2
+local pieceF3
+local pieceF4
+local pieceF5
+local pieceF6
+local pieceF7
+local pieceF8
+local pieceF9
+
+---------------------------------------
+--PUZZLE PIECES
+---------------------------------------
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+ 
+-- create()
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
+ 
+end
+ 
+ 
+-- show()
+function scene:show( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is still off screen (but is about to come on screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
+ 
+    end
+end
+ 
+ 
+-- hide()
+function scene:hide( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
+ 
+    end
+end
+ 
+ 
+-- destroy()
+function scene:destroy( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
+ 
+end
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+ 
+return scene
